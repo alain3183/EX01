da307ddd4be6c0fece1187a34d1f89d272b8262b
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-16 09:56:26 -0500

    Initial Commit

---
+++ EX01/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  EX01
+//
+//  Created by Alain Malenfant on 11/16/2013.
+//  Copyright Alain Malenfant 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ EX01/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  EX01
+//
+//  Created by Alain Malenfant on 11/16/2013.
+//  Copyright Alain Malenfant 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
